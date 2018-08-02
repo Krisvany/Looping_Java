79be5a9c26c38a096e6298145797267547eae0c2
User <User@User>
2018-08-02 09:38:02 +0700

    BILANGAN GANJIL GENAP

---
+++ src/j08/Contoh3.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j08;
+
+/**
+ *
+ * @author User
+ */
+import java.util.Scanner;
+public class Contoh3 {
+    //FOR
+    int x = 1;
+    
+  for(x = 1; x <= 20; x+=2)
+         
+    {
+        System.out.println(x +" ");
+    }
+    //MENAMPILKAN 10  BILANGAN GENAP PERTAMA 
+    for(x = 2; x <= 2;x+=2)
+    {
+        System.out.print(x + "");
+    }
+    
+    //CARA 2
+    //MENAMPILKAN 10 BILANGAN GANJIL PERTAMA
+    for(x =1;x<= 20;x++)
+    {
+        if(x % 2 == 1)
+        {
+            System.out.print(x + "");
+            
+        }
+    }
+// MENAMPILKAN BILANGAN GENAO
+    for (x = 1;x <= 20; x++)
+    {
+        if(x % 2 == 0)
+        {
+            System.out.print(x + "");
+        }
+        
+        }
+    }
